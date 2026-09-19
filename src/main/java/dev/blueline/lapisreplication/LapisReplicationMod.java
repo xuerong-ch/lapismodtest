ce4bd3ffe71cfdca9e5d7363a7bbb592ea228bbe
xuerong-ch <xuerong.ch@gmail.com>
2026-09-19 23:34:58 +0200

    Add src/main/java/dev/blueline/lapisreplication/LapisReplicationMod.java

---
+++ src/main/java/dev/blueline/lapisreplication/LapisReplicationMod.java
@@ -0,0 +1,25 @@
+package dev.blueline.lapisreplication;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(LapisReplicationMod.MOD_ID)
+public final class LapisReplicationMod {
+    public static final String MOD_ID = "lapisreplication";
+
+    public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS =
+            DeferredRegister.create(Registries.RECIPE_SERIALIZER, MOD_ID);
+
+    static {
+        RECIPE_SERIALIZERS.register("lapis_replication", () -> LapisReplicationRecipe.SERIALIZER);
+    }
+
+    public LapisReplicationMod(IEventBus modEventBus) {
+        RECIPE_SERIALIZERS.register(modEventBus);
+        NeoForge.EVENT_BUS.addListener(CraftingEvents::onItemCrafted);
+    }
+}
